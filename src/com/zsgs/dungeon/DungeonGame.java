1964017d132663ac484a62c401e06feb0b070a79
surya4186 <mjsurya00@gmail.com>
2023-12-23 12:39:55 +0530

    level2

--- src/com/zsgs/dungeon/DungeonGame.java
+++ src/com/zsgs/dungeon/DungeonGame.java
@@ -3,12 +3,10 @@ package com.zsgs.dungeon;
 import java.util.Scanner;
 
 public class DungeonGame {
-	public static void numberOfSteps(char[][] charArray, int addventurerRow, int addventurercol, int goldRow,
-			int goldCol) {
-		int step1 = Math.abs(addventurerRow - goldRow);
-		int step2 = Math.abs(addventurercol - goldCol);
-		System.out.print("min steps :");
-		System.out.println(step1 + step2);
+	public static int numberOfSteps(char[][] charArray, int row, int col, int goldRow, int goldCol) {
+		int step1 = Math.abs(row - goldRow);
+		int step2 = Math.abs(col - goldCol);
+		return step1 + step2;
 
 	}
 
@@ -19,17 +17,27 @@ public class DungeonGame {
 		System.out.println("Enter the Array col");
 		int col = scan.nextInt();
 		System.out.println("Position Of adventure:");
-		int addventurerRow = scan.nextInt();
-		int addventurercol = scan.nextInt();
+		int adventurerRow = scan.nextInt();
+		int adventurercol = scan.nextInt();
+		System.out.println("Position Of Monster:");
+		int monsterRow = scan.nextInt();
+		int monsterCol = scan.nextInt();
 		System.out.println("Position Of Gold:");
 		int goldRow = scan.nextInt();
 		int goldCol = scan.nextInt();
 		char[][] charArray = new char[row][col];
 
-		charArray[addventurerRow - 1][addventurercol - 1] = 'A';
+		charArray[adventurerRow - 1][adventurercol - 1] = 'A';
 		charArray[goldRow - 1][goldCol - 1] = 'G';
+		charArray[monsterRow - 1][monsterCol - 1] = 'M';
 
-		numberOfSteps(charArray, addventurerRow, addventurercol, goldRow, goldCol);
+		int adventurer = numberOfSteps(charArray, adventurerRow, adventurercol, goldRow, goldCol);
+		int monster = numberOfSteps(charArray, monsterRow, monsterCol, goldRow, goldCol);
+		if (adventurer >= monster) {
+			System.out.println("No possible solution");
+		} else {
+			System.out.println(" Min Number of steps: " + adventurer);
+		}
 
 	}
 
